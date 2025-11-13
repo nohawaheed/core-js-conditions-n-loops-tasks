4ed54d31d0a4a0952fc12b6d33324b70f9b20248
nohawaheed <nohawaheed.1991@gmail.com>
2025-11-14 01:46:13 +0200

    feat: add shuffleChar implementation

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -474,8 +474,25 @@ function sortByAsc(arr) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let string = str;
+  for (let i = 0; i < iterations; i += 1) {
+    let oddChars = '';
+    let evenChars = '';
+    for (let j = 0; j < string.length; j += 1) {
+      if (j % 2 !== 0) {
+        oddChars += string[j];
+      } else {
+        evenChars += string[j];
+      }
+    }
+    string = evenChars + oddChars;
+    if (string === str) {
+      const completedIterations = i + 1;
+      return shuffleChar(str, iterations % completedIterations);
+    }
+  }
+  return string;
 }
 
 /**
